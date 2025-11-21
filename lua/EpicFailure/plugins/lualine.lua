6fc0ee772871ed8bb8e01920a18cc81b862c904e
EpicFailure-afk <as3704921@gmail.com>
2025-11-21 08:35:32 +0200

    Plugins

--- lua/EpicFailure/plugins/lualine.lua
+++ lua/EpicFailure/plugins/lualine.lua
@@ -1,21 +1,19 @@
-
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local lualine = require("lualine")
-    local lazy_status = require("lazy.status")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
     local colors = {
-      blue = "#89b4fa",
-      green = "#a6e3a1",
-      violet = "#cba6f7",
-      yellow = "#f9e2af",
-      red = "#f38ba8",
-      fg = "#cdd6f4",
-      bg = "#1e1e2e",
-      inactive_bg = "#313244",
-      semilightgray = "#bac2de",
+      blue = "#65D1FF",
+      green = "#3EFFDC",
+      violet = "#FF61EF",
+      yellow = "#FFDA7B",
+      red = "#FF4A4A",
+      fg = "#c3ccdc",
+      bg = "#112638",
+      inactive_bg = "#2c3043",
     }
 
     local my_lualine_theme = {
@@ -51,18 +49,17 @@ return {
       },
     }
 
+    -- configure lualine with modified theme
     lualine.setup({
       options = {
         theme = my_lualine_theme,
-        section_separators = "",
-        component_separators = "",
       },
       sections = {
         lualine_x = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#fab387" },
+            color = { fg = "#ff9e64" },
           },
           { "encoding" },
           { "fileformat" },
