7f2a858606cc12e59a5016bd70b7aabd60d43eda
EpicFailure-afk <as3704921@gmail.com>
2025-11-21 07:26:41 +0200

    Adding three plugins
    lualine, auto-session and bufferline

--- lua/EpicFailure/lazy.lua
+++ lua/EpicFailure/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("EpicFailure.plugins")
+require("lazy").setup("EpicFailure.plugins", {
+  change_detection = {
+    notify = false,
+  },
+})
